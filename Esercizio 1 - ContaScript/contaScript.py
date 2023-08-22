36374d449fca5abbb30dad6680054b514fc19c18
Giglus <98102601+Giglus@users.noreply.github.com>
2023-08-22 20:13:10 +0200

    Esercizio 1 - ContaScript
    "Scrivi uno script bash o python che conta il numero di file script in una directory raggruppandoli in base allo shebang interpreter."

---
+++ Esercizio 1 - ContaScript/contaScript.py
@@ -0,0 +1,19 @@
+import os
+from collections import defaultdict
+
+def contaScript(directory):
+    interpreter_count = defaultdict(int)
+
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".sh"):
+                filepath = os.path.join(root, filename)
+                with open(filepath, "r") as file:
+                    first_line = file.readline().strip()
+                    if first_line.startswith("#!"):
+                        interpreter_count[first_line] += 1
+
+    for interpreter, count in interpreter_count.items():
+        print(f"{count} {interpreter}")
+
+contaScript("/usr/bin")
